3da4b6bfed54f234637557c68d44976141e642ae
Michal Borsky <borsky@terra.ru.is>
2020-10-23 11:51:51 +0000

    Added local/data_prep.sh v1.0

---
+++ local/data_prep.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# RU 2020 MB
+
+set -o errexit
+
+function error_exit () {
+  echo -e "$@" >&2; exit 1;
+}
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <src-dir> <lang> <dst-dir>"
+  echo "e.g.: $0 /home/staff/borsky/OpenASR2020/data openasr20_amharic data/"
+  exit 1
+fi
+
+src=$1
+lang=$2
+dst=$3
+
+for subset in build dev; do
+   echo "Processing $src/$lang/$subset"
+
+   dst_sub=$dst/${lang}_${subset}
+   mkdir -p $dst/${lang}_${subset} || exit 1;
+   mkdir -p $dst/${lang}_${subset}/tmp || exit 1;
+   [ ! -d $src/$lang/$subset ] && echo "$0: no such directory $src/$lang/$subset" && exit 1;
+
+   # Some prep
+   find $src/$lang/$subset/audio -type f | sort > $dst_sub/tmp/audio.lst
+   find $src/$lang/$subset/transcription -type f | sort > $dst_sub/tmp/trans.lst
+
+   # wav.scp
+   while IFS= read -r line; do
+      utt=$(echo $line | cut -d'_' -f5- | sed 's|\..*||')
+      if [[ $line =~ ".sph" ]]; then
+         echo "$utt sph2pipe -f rif -p $line |"
+      elif [[ $line =~ ".wav" ]]; then
+         echo "$utt sox $line -t wav -e signed-integer -r 8000 -b 16 - |"
+      fi
+   done < $dst_sub/tmp/audio.lst > $dst_sub/wav.scp
+
+   # Text segments
+   while IFS= read -r file; do
+      utt=$(echo $file | cut -d'_' -f5- | sed 's|\..*||')
+      beg=$(head -n 1 $file | sed -e 's|\[\(.*\)\]|\1|')
+      text="<no-speech>"
+      idx=0
+      while IFS= read -r line; do
+         if [[ $line =~ "[" ]]; then
+            end=$(echo $line| sed -e 's/\[\(.*\)\]/\1/')
+            if  ! [[ $text == "<no-speech>" ]]; then
+                echo ${utt}_$(printf "%03d" $idx) $utt $beg $end $text
+                let "idx+=1"
+            fi
+            beg=$end
+         else
+            text=$line
+         fi
+      done < $file
+   done < $dst_sub/tmp/trans.lst > $dst_sub/tmp/text_segments
+
+   # Segments, text, utt2spk, spk2utt
+   cat $dst_sub/tmp/text_segments | cut -d' ' -f1-4 > $dst_sub/segments
+   cat $dst_sub/tmp/text_segments | cut -d' ' -f1,5- > $dst_sub/text
+   cat $dst_sub/tmp/text_segments | cut -d' ' -f1,2 > $dst_sub/utt2spk
+   utils/utt2spk_to_spk2utt.pl < $dst_sub/utt2spk > $dst_sub/spk2utt
+
+   utils/validate_data_dir.sh --no-feats $dst_sub || exit 1;
+   echo "$0: successfully prepared data in $dst_sub"
+done
