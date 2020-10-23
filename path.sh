4f8423a226a55e26df082777c267d1d22426338d
Robert Kjaran <robert@kjaran.com>
2020-10-23 12:51:14 +0000

    Add sph2pipe to path

--- path.sh
+++ path.sh
@@ -5,5 +5,11 @@ else
 fi
 . "$KALDI_ROOT/tools/config/common_path.sh"
 . "$KALDI_ROOT/tools/env.sh"
-export PATH=$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/pocolm/scripts:$PWD/utils:$PWD/steps:$PATH
+export PATH=\
+$KALDI_ROOT/tools/openfst/bin:\
+$KALDI_ROOT/tools/pocolm/scripts:\
+$PWD/utils:\
+$PWD/steps:\
+$KALDI_ROOT/tools/sph2pipe_v2.5:\
+$PATH
 export SCRATCH_ROOT=/export/lr02/fast/work/openasr20
--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # Init of a run script
 
-data=/home/staff/borsky/OpenASR2020/data
+if [[ $(hostname) == lr* ]]; then
+  data=/export/lr01/lr-backup/corpora/openasr2020
+else
+  data=/home/staff/borsky/OpenASR2020/data
+fi
 work_dir=/home/staff/borsky/OpenASR2020/s5
+
 lang="openasr20_amharic"
 stage=0
+nj=1
 
 . ./cmd.sh
 . ./path.sh
@@ -16,13 +22,13 @@ echo "                		Data Prep			                "
 echo ============================================================================
 
 if [ $stage -le 0 ]; then
-   local/data_prep.sh $data $lang $work_dir/data
+   local/data_prep.sh $data $lang data/
    echo "Data prep done!"
 fi
 
 if [ $stage -le 1 ]; then
    for x in build dev; do
-     steps/make_mfcc.sh --cmd "$train_cmd" --nj 1 data/${lang}_${x} exp/log/make_mfcc exp/mfcc || exit 1;
+     steps/make_mfcc.sh --cmd "$train_cmd" --nj $nj data/${lang}_${x} exp/log/make_mfcc exp/mfcc || exit 1;
      steps/compute_cmvn_stats.sh data/${lang}_${x} exp/log/make_mfcc exp/mfcc || exit 1;
    done
 fi
