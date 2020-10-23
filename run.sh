969ae400a26fd30c60fe218daf27d370c9824c06
Robert Kjaran <robert@kjaran.com>
2020-10-23 16:29:55 +0000

    Add mono and triphone system training

--- run.sh
+++ run.sh
@@ -37,3 +37,57 @@ if [ $stage -le 2 ]; then
   local/prep_lang.sh $data $lang data/
 fi
 
+if [ $stage -le 3 ]; then
+echo ============================================================================
+echo "          Train mono system                                               "
+echo ============================================================================
+
+  steps/train_mono.sh --nj $nj --cmd "$train_cmd" \
+                      data/${lang}_build data/lang_$lang exp/$lang/mono || exit 1
+  utils/mkgraph.sh data/lang_${lang}_2g exp/$lang/mono \
+                   exp/$lang/mono/graph_2g || exit 1
+
+  for dset in build dev; do
+    steps/decode.sh --nj $nj --cmd "$decode_cmd" \
+                    exp/$lang/mono/graph_2g data/${lang}_$dset \
+                    exp/$lang/mono/decode_2g_$dset &
+  done
+fi
+
+if [ $stage -le 4 ]; then
+  echo ============================================================================
+  echo "          Train tri delta+deltadelta system                               "
+  echo ============================================================================
+  steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+                    data/${lang}_build data/lang_$lang \
+                    exp/$lang/mono exp/$lang/mono_ali || exit 1
+  steps/train_deltas.sh --nj $nj --cmd "$train_cmd" \
+                        2000 20000 \
+                        data/${lang}_build data/lang_$lang \
+                        exp/$lang/mono_ali exp/$lang/tri1 || exit 1
+fi
+
+if [ $stage -le 5 ]; then
+  echo ============================================================================
+  echo "          Train tri LDA+MLLT system                                       "
+  echo ============================================================================
+  steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+                    data/${lang}_build data/lang_$lang \
+                    exp/$lang/tri1 exp/$lang/tri1_ali || exit 1
+  steps/train_lda_mllt.sh --nj $nj --cmd "$train_cmd" \
+                          --splice-opts "--left-context=5 --right-context=5" \
+                          4000 50000 \
+                          data/${lang}_build data/lang_$lang \
+                          exp/$lang/tri1_ali exp/$lang/tri2 || exit 1
+
+  utils/mkgraph.sh data/lang_${lang}_2g exp/$lang/tri2 \
+                   exp/$lang/tri2/graph_2g || exit 1
+
+  for dset in build dev; do
+    steps/decode.sh --nj $nj --cmd "$decode_cmd" \
+                    exp/$lang/mono/graph_2g data/${lang}_$dset \
+                    exp/$lang/mono/decode_2g_$dset &
+  done
+fi
+
+wait
