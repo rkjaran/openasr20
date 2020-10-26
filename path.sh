9f6b017e94ed052e1dd6a0ab705cc7ecebf11985
Michal Borsky <borsky@terra.ru.is>
2020-10-26 16:15:57 +0000

    modified to work on terra.hir.is for user borsky

--- path.sh
+++ path.sh
@@ -1,7 +1,7 @@
 if [[ $(hostname) == lr01* ]] || [[ $(hostname) == lr02* ]]; then
   export KALDI_ROOT="/home/rkjaran/src/laeknaromur/x-romur/kaldi"
 else
-  export KALDI_ROOT=${KALDI_ROOT:-$PWD/../../../kaldi}
+  export KALDI_ROOT="/opt/kaldi"
 fi
 . "$KALDI_ROOT/tools/config/common_path.sh"
 . "$KALDI_ROOT/tools/env.sh"
@@ -12,5 +12,8 @@ $PWD/utils:\
 $PWD/steps:\
 $KALDI_ROOT/tools/sph2pipe_v2.5:\
 $PATH
-export SCRATCH_ROOT=/export/lr02/fast/work/openasr20
+if [[ $(hostname) == lr01* ]] || [[ $(hostname) == lr02* ]]; then
+   export SCRATCH_ROOT=/export/lr02/fast/work/openasr20
+else
+   export SCRATCH_ROOT=/scratch/borsky/openasr20
 export LC_ALL=C
